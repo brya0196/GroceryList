b771c063c941f23d9956f3ccd50d7be67b528d3c
Bryan Astacio <brya0196@gmail.com>
2018-07-12 10:16:06 -0400

    added the angular router to the app and set the params

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
-var app = angular.module('groceryListApp', []);
+var app = angular.module('groceryListApp', ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: 'views/groceryList.html',
+        controller: "GroceryListItemsController"
+    })
+    .when("/addItem", {
+        templateUrl: 'views/addItem.html',
+        controller: "GroceryListItemsController"
+    })
+    .when("/addItem/:id", {
+        templateUrl: 'views/addItem.html',
+        controller: "GroceryListItemsController"
+    })
+    .otherwise({
+        redirectTo: "/"
+    })
+})
 
 app.controller("HomeController", ["$scope", function ($scope) {
     $scope.appTitle = "Grocery List";
 }]);
 
-app.controller("GroceryListItemsController", ["$scope", function ($scope) {
+app.controller("GroceryListItemsController", ["$scope", "$routeParams", function ($scope, $routeParams) {
     $scope.groceryItems = [
         {completed: true, itemName: 'milk', date: '2014-10-01'},
         {completed: true, itemName: 'cookies', date: '2014-10-01'},
@@ -15,4 +34,6 @@ app.controller("GroceryListItemsController", ["$scope", function ($scope) {
         {completed: true, itemName: 'eggs', date: '2014-10-04'},
         {completed: true, itemName: 'textiles', date: '2014-10-04'},
     ];
+
+    $scope.rp = "Route parameter value: " + $routeParams.id;
 }]);
